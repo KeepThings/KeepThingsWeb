b66bf3ec0306194c0d30618b9a867949ddc74ded
FruitGLemon <nils.gabriel.braun@gmail.com>
2018-10-23 20:52:00 +0200

    first tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+interface DataD {
+    object: String;
+}
 export class AppComponent {
-  title = 'KeepThings';
+    constructor( private http: HttpClient) { }
+    title = 'KeepThings';
+    obj;
+    fetchData() {
+        this.obj = this.http.get<DataD>('http://localhost:1234/api/file.json');
+        this.http.get<DataD>('http://localhost:1234/api/file.json').subscribe(res =>{this.obj = res});
+
+    }
+
+
 }
