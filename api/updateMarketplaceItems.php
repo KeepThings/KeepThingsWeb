901ca54f8916ecf39c739edc21a136fd2216bbf5
NoSkillSwagger <40427797+NoSkillSwagger@users.noreply.github.com>
2018-11-09 15:47:38 +0100

    Add files via upload

---
+++ api/updateMarketplaceItems.php
@@ -0,0 +1,26 @@
+<?php 
+include("commands.php");
+if(isset($_GET['IID']) && $_GET['IID'] != null){
+
+	$res = dbSelect("SELECT * FROM Marketplace_Items WHERE ITEM_ID = '".$_GET['IID']."'");
+	$dsatz = $res->fetch_object();
+	$rows = $res->num_rows;
+	
+	if($rows==0 || null){
+		echo '"result": {"success": false}';
+	} else{
+	dbSelect("UPDATE Marketplace_Items SET 
+	
+	ITEM_ID = '".$_GET['IID']."',
+	ITEM_NAME = '".$_GET['ITEM_NAME']."',
+	ITEM_DESC = '".$_GET['ITEM_DESC']."',
+	USERNAME = '".$_GET['UserIdToName($dsatz->USER_ID)']."',
+	BORROWER = '".$_GET['BORROWER']."',
+	DATE_FROM = '".$_GET['DATE_FROM']."',
+	DATE_TO = '".$_GET['DATE_TO']."',
+	
+	WHERE ITEM_ID = '".$_GET['IID']."'");)
+	);
+	echo json_encode(array('"result": {"success": true}'));
+	}
+}
