e3372932d6aa501d20406d82f979bc50d0849e39
NoSkillSwagger <40427797+NoSkillSwagger@users.noreply.github.com>
2018-11-09 16:21:47 +0100

    Add files via upload
    added updateUsers, updateUserItems
    fixed updateMarketplaceItems

--- api/updateMarketplaceItems.php
+++ api/updateMarketplaceItems.php
@@ -1,7 +1,6 @@
 <?php 
 include("commands.php");
 if(isset($_GET['IID']) && $_GET['IID'] != null){
-
 	$res = dbSelect("SELECT * FROM Marketplace_Items WHERE ITEM_ID = '".$_GET['IID']."'");
 	$dsatz = $res->fetch_object();
 	$rows = $res->num_rows;
@@ -9,8 +8,7 @@ if(isset($_GET['IID']) && $_GET['IID'] != null){
 	if($rows==0 || null){
 		echo '"result": {"success": false}';
 	} else{
-	dbSelect("UPDATE Marketplace_Items SET 
-
+	dbUpdate("UPDATE Marketplace_Items SET 
 	ITEM_NAME = '".$_GET['ITEM_NAME']."',
 	ITEM_DESC = '".$_GET['ITEM_DESC']."',
 	USERNAME = '".UserIdToName($_GET['USER_ID'])."',
@@ -19,7 +17,7 @@ if(isset($_GET['IID']) && $_GET['IID'] != null){
 	DATE_TO = '".$_GET['DATE_TO']."',
 	
 	WHERE ITEM_ID = '".$_GET['IID']."'");
-
 	echo json_encode(array('"result": {"success": true}'));
 	}
 }
+?>
